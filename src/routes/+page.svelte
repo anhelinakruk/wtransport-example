0625a0b7b88d6fcf72cae537c773c179fb7eb0ea
Anhelina Kruk <krukanhelina06@gmail.com>
2025-02-17 13:32:27 +0100

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,58 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let messages: (string | number)[] = [];
+
+  async function connectWebTransport() {
+      try {
+          const transport = new WebTransport("https://localhost:4433/");
+          await transport.ready;
+
+          console.log("Connected to WebTransport server");
+
+          const uds = transport.incomingUnidirectionalStreams;
+          const reader = uds.getReader();
+
+          console.log(uds); 
+          console.log(reader);
+
+          while (true) {
+            const { done, value } = await reader.read();
+            if (done) {
+              break;
+            }
+            // value is an instance of WebTransportReceiveStream
+            await readData(value);
+          }
+      } catch (error) {
+          console.error("WebTransport connection error:", error);
+      }
+  }
+
+  async function readData(receiveStream: any) {
+    const reader = receiveStream.getReader();
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+            break;
+        }
+
+        const text = new TextDecoder().decode(value);
+        
+        const number = parseInt(text, 10);
+        if (!isNaN(number)) {
+            console.log("Number:", number);
+            messages = [...messages, number];
+        } else {
+            console.warn("Failed to parse number:", text);
+        }
+    }
+}
+
+  connectWebTransport();
+</script>
+
+<h1>WebTransport Client</h1>
+<ul>
+  {#each messages as message}
+      <li>{message}</li>
+  {/each}
+</ul>
